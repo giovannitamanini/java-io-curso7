4e34fa5f7e63d14d3582c12fb7eb6731c4876a50
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-11-16 13:42:56 -0300

    Usando a classe PrintWriter

--- java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -1,19 +1,16 @@
 package br.com.alura.java.io.teste;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 
 public class TesteEscritaPrintStreamPrintWriter {
 
     public static void main(String[] args) throws IOException {
 
-        PrintStream ps = new PrintStream("lorem2.txt");
+        PrintWriter pw = new PrintWriter("lorem2.txt");
 
-        ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
-        ps.println("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam");
+        pw.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        pw.println("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam");
 
-        ps.close();
+        pw.close();
     }
 }

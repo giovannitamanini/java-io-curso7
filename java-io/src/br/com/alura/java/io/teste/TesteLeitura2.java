8a614b3fd35dbac193b0e23c828f7468c136a94d
Giovanni Tamanini <giovanni.tnini@gmail.com>
2022-11-17 16:31:43 -0300

    Utilizando a classe Properties e criando arquivo de teste conf.properties

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -8,22 +8,22 @@ public class TesteLeitura2 {
 
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(new File("contas.csv"));
+        Scanner scanner = new Scanner(new File("conta.csv"));
         while (scanner.hasNextLine()) {
             String linha = scanner.nextLine();
-            System.out.println(linha);
 
             Scanner linhaScanner = new Scanner(linha);
             linhaScanner.useLocale(Locale.US);
             linhaScanner.useDelimiter(",");
 
-            String valor1 = linhaScanner.next();
-            int valor2 = linhaScanner.nextInt();
-            int valor3 = linhaScanner.nextInt();
-            String valor4 = linhaScanner.next();
-            double valor5 = linhaScanner.nextDouble();
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
 
-            System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+            String valorFormatado = String.format(new Locale("pt", "BR"),"%s - %04d-%08d, %20s: %08.2f", tipoConta, agencia, numero, titular, saldo);
+            System.out.println(valorFormatado);
 
             linhaScanner.close();
 
